b2fcb3b6045af8ea516b958133e7e58a951fa45b
Xince Shi <crystalxinceshi@gmail.com>
2019-07-25 21:00:05 -0700

    listing detail page

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # haoshiyou-uniapp
 
 > a project for haoshiyou uniapp
-## Setup 
+
+## Setup
+
 1. `git clone <this repo>`
-2. `cd <dir> && npm install`  # install 
+2. `cd <dir> && npm install` # install
 3. create `.env` file with template. See npmjs.org/dotenv for reference.
 
 Ask @xinbenlv for keys.
@@ -29,7 +31,7 @@ MAIL_SMTP=
 
 ## Build Setup
 
-``` bash
+```bash
 # install dependencies
 $ npm install
 
@@ -47,42 +49,49 @@ $ npm run generate
 For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
 
 ## Features
+
 ### Bot
+
 - Utils - utility methods to be used
-  - [X] isAdmin
-  - [X] isWhitelisted
-  - [X] isGoodNickname
-  - [X] shouldCare
-  - [X] isBlacklisted
-  - [X] getRelatedUsers
+
+  - [x] isAdmin
+  - [x] isWhitelisted
+  - [x] isGoodNickname
+  - [x] shouldCare
+  - [x] isBlacklisted
+  - [x] getRelatedUsers
 
 - Room Action
-  - [X] Add to HSY Rooms #tested #tested
-  - [X] (Safe)-kick from HSY Rooms #tested
-  - [X] Blacklist + Extend #tested
-  - [X] Kick #tested
-  - [X] Make Room Announcement #tested
-  - [X] (maybe)Downsize #tested
-  - [X] Record invitation #tested
-  - [X] Accept Friendship #tested
-  - [X] Record Friendship #tested
+
+  - [x] Add to HSY Rooms #tested #tested
+  - [x] (Safe)-kick from HSY Rooms #tested
+  - [x] Blacklist + Extend #tested
+  - [x] Kick #tested
+  - [x] Make Room Announcement #tested
+  - [x] (maybe)Downsize #tested
+  - [x] Record invitation #tested
+  - [x] Accept Friendship #tested
+  - [x] Record Friendship #tested
 
 - Intention Parsing
-  - [X] SeekInstruction
-  - [X] JoinRoom + which room 
-  - [X] PostListing
+
+  - [x] SeekInstruction
+  - [x] JoinRoom + which room
+  - [x] PostListing
 
 - Info Extraction
+
   - [3] Extract Text with haoshiyou-ai
   - [1] Record contact information
-  - [X] Download Image, downsample and upload to Cloudinary, Get ID #tested
+  - [x] Download Image, downsample and upload to Cloudinary, Get ID #tested
 
 - Other
- - [3] Downsize friendships? not so soon
- - [X] Use cronjob to post announcement
- - [X] Use npmjs.org/bottleneck to limit rate
- 
+- [3] Downsize friendships? not so soon
+- [x] Use cronjob to post announcement
+- [x] Use npmjs.org/bottleneck to limit rate
+
 ### UI
+
 - [1] Launch bot from server
 - [1] Show QR-code
 - [1] Show List
@@ -91,6 +100,7 @@ For detailed explanation on how things work, checkout [Nuxt.js docs](https://nux
 - [2] Bot status page behind admin
 
 ## MongoDB schema
+
 ```json5
 {
   // Message
@@ -99,52 +109,56 @@ For detailed explanation on how things work, checkout [Nuxt.js docs](https://nux
   },
   // MessageMeta
   MessageMeta: {
-    _id: "message.id",
+    _id: "message.id"
   },
-  
+
   // Contact
   ContactMeta: {
     _id: "contact.id",
     isAdmin: "boolean",
     isWhitelisted: "boolean",
     isBlacklisted: "boolean",
-    invited: [{
-      inviteeId: "string", 
-      timestamp: "datetime", 
-      roomId: "string" 
-    }],
-    invitedBy: [{
-      inviterId: "string", 
-      timestamp: "datetime", 
-      roomId: "string" 
-    }],
+    invited: [
+      {
+        inviteeId: "string",
+        timestamp: "datetime",
+        roomId: "string"
+      }
+    ],
+    invitedBy: [
+      {
+        inviterId: "string",
+        timestamp: "datetime",
+        roomId: "string"
+      }
+    ],
     blacklistedRecords: [
-      { 
+      {
         adminId: "string",
         timestamp: "datetime",
         direct: "boolean", // true for direct blacklist, false for indirect blacklist (caused by related user being blacklisted)
         causeContactId: "string",
         degreeOfExtension: "number"
-      },
+      }
     ],
     friendship: [
-      { 
-        type:  "Enum<string>",  // {"friend", "unfriend"}
+      {
+        type: "Enum<string>", // {"friend", "unfriend"}
         timestamp: "datetime"
       }
     ]
   },
-  
+
   // ContactMeta
   Contact: {
     _id: "contact.id"
   },
-  
+
   // HsyListing
   HsyListing: {
     _id: "type:contact.id", // e.g. wxId:wxid_wl56wlfbwn512
     imageIds: "string",
-    idType: "Enum<string>",  // {wxId, wxNick}
+    idType: "Enum<string>", // {wxId, wxNick}
     content: "string",
     title: "string",
     price: "string",
@@ -156,12 +170,12 @@ For detailed explanation on how things work, checkout [Nuxt.js docs](https://nux
       city: "string",
       state: "string",
       lat: "number",
-      lng: "number",
-    },
+      lng: "number"
+    }
   }
 }
-
 ```
+
 ###
 
 ```.env
@@ -175,7 +189,8 @@ AXIOS_BASE_URL=
 ```
 
 ### Next Generation
-  // intent framework
-  // message comes in, getting the intent, or collectively getting intent
-  // when more intent is getting in emit intent and intent state
-  // intent, knowledge, intent related knowledge
+
+// intent framework
+// message comes in, getting the intent, or collectively getting intent
+// when more intent is getting in emit intent and intent state
+// intent, knowledge, intent related knowledge
--- pages/listing/_id.vue
+++ pages/listing/_id.vue
@@ -1,25 +1,124 @@
 <template>
 <section>
-  <div>
-    {{ $route.params.id }}
-  </div>
-  <div>
-    {{ listing }}
+  <div class="bg-light">
+    <div class="col-10 mx-auto bg-white pb-5">
+      <div class="listing-navbar">
+        <nuxt-link to="/"><i class="fa fa-angle-left fa-3x" aria-hidden="true"></i></nuxt-link>
+      </div>
+      <div class="listing-body">
+        <div class="listing-images">
+          <div v-if="listing.imageIds">
+            <b-carousel id="carousel-1" v-model="slide" controls indicators :interval="0" background="rgba(0, 0, 0, 0.2)" img-width="400" img-height="300" style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
+              <!-- <b-carousel-slide v-for="imageId in listing.imageIds" caption="First slide" img-src="`http://res.cloudinary.com/xinbenlv/image/upload/c_fill,g_north,w_400,h_400,g_center/${imageId}.jpg`"></b-carousel-slide> -->
+              <b-carousel-slide v-for="imageId in listing.imageIds" v-bind:key="imageId.id">
+                <img @click="show=true" slot="img" class="d-block img-fluid mx-auto" :src="`http://res.cloudinary.com/xinbenlv/image/upload/c_fill,g_north,g_center/${imageId}.jpg`" alt="image slot" style="touch-action: pan-y; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); height: 300px; width: 300px">
+                <b-modal v-model="show" :hide-footer=true>
+                  <img class="d-block img-fluid mx-auto" :src="`http://res.cloudinary.com/xinbenlv/image/upload/c_fill,g_north,g_center/${imageId}.jpg`" alt="img-" style="touch-action: pan-y; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                </b-modal>
+              </b-carousel-slide>
+            </b-carousel>
+          </div>
+          <div v-else class="mx-auto" style="width: 400px">
+            <img itemprop="image" alt="" src="/img/no-photo-placeholder.jpg" style="touch-action: pan-y; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); width: 400px; height: 300px">
+          </div>
+        </div>
+        <div class="listing-title ">
+          <h3 v-if="listing.title"> {{ listing.title }}</h3>
+          <h3 v-else > 招租 </h3>
+          <p class="text-muted"> {{ listing.updated | moment("from") }} </p>
+        </div>
+        <div v-if="listing.geo" class="listing-map ">
+          <ListingMapView :listing="listing" />
+        </div>
+        <div v-if="listing.city" class="listing-city text-body font-weight-bold col-12">
+          {{ listing.city}}
+        </div>
+        <div class="listing-content col-12 mt-4">
+          <h3>描述</h3>
+          <p>{{ listing.content }}</p>
+          <hr class="my-4">
+          <h3>联系房东</h3>
+          <div>
+            <i class="fa fa-cube" aria-hidden="true"></i> 
+            <span class="col-1"> 电话</span> 
+            <span v-if="listing.phone" class="col-1">{{ listing.phone }}</span>
+            <span v-else class="col-1">N/A</span>
+          </div>
+          <div>
+            <i class="fa fa-cube" aria-hidden="true"></i>
+            <span class="col-1"> 邮箱</span> 
+            <span v-if="listing.email" class="col-1">{{ listing.email }}</span>
+            <span v-else class="col-1">N/A</span>
+          </div>
+          <div>
+            <i class="fa fa-cube" aria-hidden="true"></i>
+            <span class="col-1"> 微信</span> 
+            <span v-if="listing.wechat" class="col-1">{{ listing.wechat }}</span>
+            <span v-else class="col-1">N/A</span>
+          </div>
+
+        </div>
+        <div class="listing-price fixed-bottom text-center mx-auto col-10 bg-white border-top shadow">
+          <h3 v-if="listing.price"><i class="fa fa-usd" aria-hidden="true"></i>{{ listing.price }}/月</h3>
+          <h3 v-else>价格待议</h3>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 
 </template>
 <script>
-  export default {
-    async validate ({ params, $axios }) {
-      const listing = await $axios.$get(`/api/v1/HsyListing/${params.id}`);
-      console.log(`Returned Listing = ${listing}`);
-      return listing != null;
+import ListingMapView from "~/components/ListingMapView.vue";
+import moment from "moment";
+
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+      show: false
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
     },
-    async asyncData({ params, $axios }) {
-      const listing = await $axios.$get(`/api/v1/HsyListing/${params.id}`);
-      return { listing }
-    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    }
+  },
+  components: {
+    ListingMapView
+  },
+  filters: {
+    moment: function(date) {
+      return moment(date).fromNow();
+    }
+  },
+  async validate({ params, $axios }) {
+    const listing = await $axios.$get(`/api/v1/HsyListing/${params.id}`);
+    console.log(`Returned Listing = ${listing}`);
+    return listing != null;
+  },
+  async asyncData({ params, $axios }) {
+    const listing = await $axios.$get(`/api/v1/HsyListing/${params.id}`);
+    return { listing };
   }
+};
 </script>
-<style></style>
+<style>
+.listing-navbar {
+  color: #6c6c6c;
+  padding-left: 10px;
+}
+.listing-city {
+  margin-top: -30px;
+  background-color: rgba(255, 255, 255, 0.7);
+  position: relative;
+  height: 30px;
+}
+.listing-price {
+  height: 50px;
+}
+</style>
